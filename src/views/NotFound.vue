07dd60be9a98029c69e4a1f478084ee1764bd99b
WE5T3 <1024427426@qq.com>
2021-09-17 02:26:59 +0800

    添加 404 页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+  404 Not Found
+  <br>
+  <span>当前页面不存在,请检查网址是否正确</span>
+  <br>
+  <br>
+  <router-link to = "/">返回首页</router-link>
+</div>
+</template>
+
+<script lang = "ts">
+export default {
+  name: ""
+}
+</script>
+
+<style lang = "scss" scoped>
+a {
+  text-decoration: none;
+  border: #839494 1px solid;
+  padding: 5px;
+  margin: 10px;
+  font-weight: bold;
+  color: #2c3e50;}
+</style>
