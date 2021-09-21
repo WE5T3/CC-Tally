7fc75f857152034ec4a745ad96cb89d7203f8234
WE5T3 <1024427426@qq.com>
2021-09-21 19:15:45 +0800

    完成Types组件

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,15 +1,32 @@
 <template>
   <div class = "types">
-    <div class = "expense">支出</div>
-    <div class = "income">收入</div>
+    <div class = "expense" :class="type==='-' && 'selected'"
+         @click="selectType('-')">支出</div>
+    <div class = "income"  :class="type==='+' && 'selected'"
+         @click="selectType('+')">收入</div>
   </div>
 </template>
 
-<script lang = "ts">
-export default {
-  name: "Types"
+<script lang="ts">
+import Vue from 'vue'
+import {Component, Prop} from "vue-property-decorator";
+
+@Component
+export default class Types extends Vue{
+  type='-' //'-'表示支出, '+'表示收入
+
+  selectType(type: string) { //type只能是'-'和'+'中的一个
+    if(type!=='-'&&type !=='+'){
+      throw new Error('type is unknown')
+    }
+    this.type = type
+    // console.log(type)
+  }
+
 }
+
 </script>
+
 
 <style lang = "scss" scoped>
 @import "~@/assets/style/helper.scss";
@@ -27,14 +44,11 @@ export default {
     border-radius: 7px;
     margin: 0 1px;
     padding: 4px;
+    background-color: #dbe8e7;
   }
 
-  .expense {
+  .selected {
     background-color: $tfn2;
-  }
-
-  .income {
-    background-color: #cfd9d8;
   }
 
 }
