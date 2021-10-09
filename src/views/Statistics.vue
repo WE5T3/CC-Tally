dcdb36b50eb114c112a2e223753620572fd09528
WE5T3 <1024427426@qq.com>
2021-10-10 05:43:41 +0800

    自动更新options

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -4,6 +4,19 @@
       <div class = "chart-wrapper" ref = "chartWrapper">
         <Chart class = "chart" :options = "chartOptions"/>
       </div>
+      <div class = "month">
+        <el-date-picker
+            class = "month-picker"
+            v-model = "month1"
+            size = "small"
+            :editable = "false"
+            :clearable = 'false'
+            value-format = "yyyy-MM"
+            :picker-options = "pickerOptions"
+            type = "month"
+            placeholder = "选择月">
+        </el-date-picker>
+      </div>
     </Layout>
   </div>
 </template>
@@ -14,14 +27,22 @@ import {Component, Vue} from 'vue-property-decorator'
 import Chart from '@/components/Chart.vue'
 import _ from "lodash"
 import dayjs from "dayjs"
+import clone from "@/lib/clone"
 
 @Component({
   components: {Chart},
 })
 export default class Statistics extends Vue {
+  month1: string = dayjs().format('YYYY-MM')
 
   beforeCreate() {
     this.$store.commit('fetchRecords')
+  }
+
+  pickerOptions = {
+    disabledDate(time) {
+      return time.getTime() > Date.now()
+    },
   }
 
   mounted() {
@@ -33,21 +54,61 @@ export default class Statistics extends Vue {
     return (this.$store.state as RootState).recordList
   }
 
+  get groupList() {
+
+    const {recordList} = this
+    const newList = clone(recordList)
+        .sort((a, b) => dayjs(b.date + ' ' + b.time).valueOf() - dayjs(a.date + ' ' + a.time).valueOf())
+    if (newList.length === 0) {return []}
+    const monthList = clone(newList).filter(item => dayjs(item.date).format('YYYY-MM') === this.month1)
+    if (monthList.length === 0) {return []}
+
+    type Result = {
+      title: string,
+      total?: number,
+      expenseTotal?: number,
+      incomeTotal?: number,
+      items: RecordItem[]
+    }[]
+
+    const result: Result = [{title: dayjs(monthList[0].date).format('YYYY-MM-DD'), items: [monthList[0]]}]
+
+    for (let i = 1; i < monthList.length; i++) {
+      const current = monthList[i]
+      const last = result[result.length - 1]
+      if (dayjs(last.title).isSame(dayjs(current.date), 'day')) {
+        last.items.push(current)
+      } else {
+        result.push({title: dayjs(current.date).format('YYYY-MM-DD'), items: [current]})
+      }
+    }
+
+    result.map(group => {
+      group.total = group.items.reduce((sum, item) => {
+        return eval(sum + (item.type + item.amount))
+      }, 0)
+    })
+    return result
+  }
+
+
   get keyValueList() {
     const today = new Date()
-    const dataList = this.recordList.map(r => _.pick(r, ['date', 'type', 'amount']))
+    const dataList = this.groupList.map(r => _.pick(r, ['title', 'total']))
+    // console.log(this.groupList)
     // console.log(dataList)
     const array: any = []
     for (let i = 0; i <= 29; i++) {
-      const date0 = dayjs(today).subtract(i, 'day').format('YYYY-MM-DD')
-      const found = _.find(this.recordList, {
-        date: date0
+      const dateString = dayjs(today).subtract(i, 'day').format('YYYY-MM-DD')
+      const found = _.find(this.groupList, {
+        title: dateString
       })
+      // console.log(found)
       array.push(
           {
-            date: date0,
-            type: found ? found.type : 0,
-            amount: found ? found.amount : 0
+            date: dateString,
+            // type: found ? found.type : 0,
+            total: found ? found.total : 0
           })
     }
     array.sort((a, b) => {
@@ -64,7 +125,7 @@ export default class Statistics extends Vue {
 
   get chartOptions() {
     const keys = this.keyValueList.map(item => item.date)
-    const amount = this.keyValueList.map(item => item.amount)
+    const amount = this.keyValueList.map(item => item.total)
     return {
       tooltip: {
         trigger: 'axis',
